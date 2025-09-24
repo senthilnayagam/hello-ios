f446be8cb3f89342bf3878ee98da6d68c286a66a
senthil nayagam <senthil.nayagam@gmail.com>
2025-09-24 15:47:04 +0530

    Initial Commit

---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by Senthil Nayagam on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
