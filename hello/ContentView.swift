2f708b71e5e00b7c6f71df625238fab989e4ea0a
senthil nayagam <senthil.nayagam@gmail.com>
2025-09-24 16:34:53 +0530

    time with. button added

--- hello/ContentView.swift
+++ hello/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showTimeAlert = false
+    @State private var currentTimeText = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Show Time") {
+                let formatter = DateFormatter()
+                formatter.dateStyle = .none
+                formatter.timeStyle = .medium
+                currentTimeText = formatter.string(from: Date())
+                showTimeAlert = true
+            }
+            .padding(.top, 12)
         }
         .padding()
+        .alert("Current Time", isPresented: $showTimeAlert) {
+            Button("OK", role: .cancel) {}
+        } message: {
+            Text(currentTimeText)
+        }
     }
 }
 
